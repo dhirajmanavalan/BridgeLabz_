ee276076d92ef0a59d45bd8d22fc5970b7ba1307
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-26 16:25:35 +0530

    default argue

--- Arguements.py
+++ Arguements.py
@@ -1,6 +1,13 @@
 # positional Arguements
 
-def add(x,y):
-    z=x**y
+# def add(x,y):
+#     z=x**y
+#     print(z)
+# add(2,3)
+
+# default Arguements
+
+def sub(x,y=0):
+    z=x+y
     print(z)
-add(2,3)
+sub(1)
