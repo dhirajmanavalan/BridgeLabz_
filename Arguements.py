52ba8d37108527d1b38fdd8a39b2470c023571a6
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-26 16:31:40 +0530

    arguements

--- Arguements.py
+++ Arguements.py
@@ -5,9 +5,29 @@
 #     print(z)
 # add(2,3)
 
-# default Arguements
+# # default Arguements
 
-def sub(x,y=0):
+# def sub(x,y=0):
+#     z=x+y
+#     print(z)
+# sub(1)
+
+#keyword Arguements
+
+def add(x,y):
     z=x+y
-    print(z)
-sub(1)
+    print("x",x)
+add(y=2,x=1)
+
+#Variable length Arguement
+
+def pizza(*toppings):
+    print(toppings)
+pizza("a","b","c")
+
+#Variable length keyword Arguement
+
+def pizza1(**toppings):
+    print(toppings)
+pizza1 (dhiraj="a",dinesh="b",monisha="c")
+
